e1724294df12be01dcb1c6858801d80183f22127
Manuel <mhanuellade309@gmail.com>
2025-08-04 08:42:09 +0100

    created a file to handle each user order

--- src/service/LoggerDetails.java
+++ src/service/LoggerDetails.java
@@ -2,7 +2,9 @@ package service;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.time.format.DateTimeFormatter;
 
 import model.Products;
 
@@ -12,7 +14,15 @@ public class LoggerDetails {
     public static void logUserOrder(String name, List<Products> cart, double totalAmount) {
         System.out.println("Logging user order details...");
         StringBuilder logEntry = new StringBuilder();
+
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
         logEntry.append(name).append(" ordered: [");
+
+            String safeName = name.replaceAll("//s+", "_").toLowerCase();
+            String safeFileName = "src/resources/order_" + safeName + ".txt";
 
            for (Products product : cart) {
             logEntry.append(product.getName())
@@ -26,14 +36,17 @@ if (!cart.isEmpty()) {
 }
 
 logEntry.append("]").append(" Total Amount: $").append(totalAmount);
+logEntry.append("\n");
+logEntry.append("Order logged at: ").append(now.format(formatter));
+logEntry.append("\n");
 
 System.out.println("Log Entry: " + logEntry.toString());
 
 
 
-try(FileWriter writer = new FileWriter("orderLog.txt", false)) {
+try(FileWriter writer = new FileWriter(safeFileName, true)) {
     writer.write(logEntry.toString() + "\n");
-    System.out.println("Order details logged successfully." );
+    System.out.println("Order details logged successfully." + safeFileName );
 }catch (IOException e) {
     System.out.println("Error writing to file: " + e.getMessage());
 }
